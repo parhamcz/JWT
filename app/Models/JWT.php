82504092a1dbc10b23a5320522d8d8ab3838e264
Parham Safaiyan <parham.safaiyann@gmail.com>
2024-04-17 11:30:02 +0330

    (*fix) removed boot method and give uuid manually.

--- app/Models/JWT.php
+++ app/Models/JWT.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Ramsey\Uuid\Uuid;
 
 class JWT extends Model
 {
@@ -13,7 +14,9 @@ class JWT extends Model
     protected $fillable = ['uuid', 'user_id', 'revoked', 'name'];
 
     protected $primaryKey = 'uuid';
-
+    protected $casts = [
+        'uuid' => 'string'
+    ];
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
--- app/Models/RefreshToken.php
+++ app/Models/RefreshToken.php
@@ -5,12 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Ramsey\Uuid\Uuid;
 
 class RefreshToken extends Model
 {
     protected $table = 'refresh_tokens';
     protected $fillable = ['uuid', 'user_id', 'name', 'revoked'];
     protected $primaryKey = 'uuid';
+    protected $casts = [
+        'uuid' => 'string'
+    ];
 
     public function user(): BelongsTo
     {
